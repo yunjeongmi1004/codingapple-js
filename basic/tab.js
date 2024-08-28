ab5fdabe7e73248e4007fa7814900041f5e96f23
yunjeongmi1004 <love_j90@naver.com>
2024-08-28 12:30:25 +0900

    이벤트 버블링 / array 와 object 자료형 비교

--- basic/tab.js
+++ basic/tab.js
@@ -4,12 +4,20 @@
 // - 모든 div에 붙은 show 클래스명 제거
 // - div0에 show 클래스명 추가
 
+// 좋은관습 : 자주쓰는 셀렉터 변수에 넣어쓰기
 let tabBtns = document.querySelectorAll('.tab-button');
-let tabCont = document.querySelectorAll('.tab-content');
+let tabConts = document.querySelectorAll('.tab-content');
 
 tabBtns.forEach((btn, index) =>{
     btn.addEventListener('click', ()=>{
-        alert(index);
+        tabBtns.forEach(otherBtn => {
+            otherBtn.classList.remove('orange');
+        });
+        tabBtns[index].classList.add('orange');
+        tabConts.forEach(othercont => {
+            othercont.classList.remove('show');
+        });
+        tabConts[index].classList.add('show');
     })
 })
 
